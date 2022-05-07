25a0e1a4e01b4ae23075183fc8199be9b8fa791e
ffluk3 <lucas.shadler@flockfreight.com>
2022-05-06 18:44:29 -0700

    fix typography

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -16,11 +16,11 @@ export default function Home() {
   return (
     <>
       <Typography variant="h2">Welcome to my Demo Remix App</Typography>
-      <Typography variant="p">
+      <Typography variant="body1">
         View the full code{" "}
         <a href="https://github.com/ffluk3/remix-trial">here</a>
       </Typography>
-      <Typography variant="p">
+      <Typography variant="body1">
         This is an "authenticated" route, simulated via a session cookie built
         into remix
       </Typography>
