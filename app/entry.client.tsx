bc3026edc00b425330cd02b87ed931138ab13723
ffluk3 <lucas.shadler@flockfreight.com>
2022-05-06 18:52:21 -0700

    unify emotion cache

--- app/entry.client.tsx
+++ app/entry.client.tsx
@@ -1,11 +1,9 @@
 import { RemixBrowser } from "@remix-run/react";
 import { hydrate } from "react-dom";
 import { CacheProvider } from "@emotion/react";
-import createEmotionCache from "@emotion/cache";
+import createEmotionCache from "./theme/create-emotion-cache";
 
-const clientSideCache = createEmotionCache({
-  key: "client-cache",
-});
+const clientSideCache = createEmotionCache();
 
 hydrate(
   <CacheProvider value={clientSideCache}>
--- app/entry.server.tsx
+++ app/entry.server.tsx
@@ -1,12 +1,11 @@
 import type { EntryContext } from "@remix-run/node";
 import { RemixServer } from "@remix-run/react";
 import { renderToString } from "react-dom/server";
-import createEmotionCache from "@emotion/cache";
+import createEmotionCache from "./theme/create-emotion-cache";
 import { CacheProvider } from "@emotion/react";
 
-const serverSideCache = createEmotionCache({
-  key: "server-cache",
-});
+const serverSideCache = createEmotionCache();
+
 export default function handleRequest(
   request: Request,
   responseStatusCode: number,
