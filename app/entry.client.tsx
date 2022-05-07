e8311f2108af2f9511b1b33788e22f9cdab5b2c3
ffluk3 <lucas.shadler@flockfreight.com>
2022-05-06 19:03:45 -0700

    full ssr with mui styling

--- app/entry.client.tsx
+++ app/entry.client.tsx
@@ -2,12 +2,17 @@ import { RemixBrowser } from "@remix-run/react";
 import { hydrate } from "react-dom";
 import { CacheProvider } from "@emotion/react";
 import createEmotionCache from "./theme/create-emotion-cache";
+import { theme } from "~/theme";
+import { CssBaseline, ThemeProvider } from "@mui/material";
 
 const clientSideCache = createEmotionCache();
 
 hydrate(
   <CacheProvider value={clientSideCache}>
-    <RemixBrowser />
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <RemixBrowser />
+    </ThemeProvider>
   </CacheProvider>,
-  document
+  document.querySelector("#root")
 );
--- app/entry.server.tsx
+++ app/entry.server.tsx
@@ -3,28 +3,52 @@ import { RemixServer } from "@remix-run/react";
 import { renderToString } from "react-dom/server";
 import createEmotionCache from "./theme/create-emotion-cache";
 import { CacheProvider } from "@emotion/react";
-import { ThemeProvider } from "@mui/material";
+import { CssBaseline, ThemeProvider } from "@mui/material";
 import { theme } from "~/theme";
+import createEmotionServer from "@emotion/server/create-instance";
 
-const serverSideCache = createEmotionCache();
-
+function renderFullPage(html: string, css: string): string {
+  return `
+    <!DOCTYPE html>
+    <html>
+      <head>
+        <title>My page</title>
+        ${css}
+        <meta name="viewport" content="initial-scale=1, width=device-width" />
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap" />
+      </head>
+      <body>
+        <div id="root">${html}</div>
+      </body>
+    </html>
+  `;
+}
 export default function handleRequest(
   request: Request,
   responseStatusCode: number,
   responseHeaders: Headers,
   remixContext: EntryContext
 ) {
+  const serverSideCache = createEmotionCache();
+
+  const { extractCriticalToChunks, constructStyleTagsFromChunks } =
+    createEmotionServer(serverSideCache);
+
   let markup = renderToString(
     <CacheProvider value={serverSideCache}>
       <ThemeProvider theme={theme}>
+        <CssBaseline />
         <RemixServer context={remixContext} url={request.url} />
       </ThemeProvider>
     </CacheProvider>
   );
 
+  const emotionChunks = extractCriticalToChunks(markup);
+  const emotionCss = constructStyleTagsFromChunks(emotionChunks);
+
   responseHeaders.set("Content-Type", "text/html");
 
-  return new Response("<!DOCTYPE html>" + markup, {
+  return new Response(renderFullPage(markup, emotionCss), {
     status: responseStatusCode,
     headers: responseHeaders,
   });
